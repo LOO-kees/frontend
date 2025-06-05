68b1eb7f937d8ec3246d5fe73d839f086a6ffd9c
LOO-kees <tlswjs16@gmail.com>
2025-06-05 15:38:21 +0900

    Update FruitsUpdate.js

--- src/component/FruitsUpdate.js
+++ src/component/FruitsUpdate.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function FruitsUpdate() {
-  //1. 변수선언 과일 정보
+  // 1. URL 파라미터에서 num 꺼내기
   const { num } = useParams();
+
+  // 2. 수정 폼 상태 선언 (초기값은 빈 문자열)
   const [form, setForm] = useState({
     num: '',
     name: '',
@@ -15,39 +17,52 @@ function FruitsUpdate() {
 
   const navigate = useNavigate();
 
-  //2. 서버측에 넘길 데이터(num)를 통신해서 성공, 실패여부 출력
+  // 3. 백엔드 Public URL 설정
+  // 로컬 개발/테스트 시:
+  // const BACKEND_URL = 'http://localhost:9070';
+  // 배포 상태에서는 CloudType Public URL 사용:
+  const BACKEND_URL = 'https://port-0-backend-mbioc25168a38ca1.sel4.cloudtype.app';
+
+  // 4. 컴포넌트 마운트 시 해당 과일(GET /fruits/:num) 정보 불러오기
   useEffect(() => {
     axios
-      .get(`http://localhost:9070/fruits/${num}`)
+      .get(`${BACKEND_URL}/fruits/${num}`)
       .then(res => {
         console.log('서버 응답값(fruit 상세):', res.data);
         setForm(res.data);
       })
-      .catch(err => console.log('조회 오류(fruit) :', err));
-  }, [num]);
+      .catch(err => console.log('조회 오류(fruit):', err));
+  }, [num, BACKEND_URL]); // BACKEND_URL을 의존성 배열에 포함
 
-  //사용자가 입력양식에 데이터를 입력했을 경우 상태 변수에 저장하기
+  // 5. 입력값이 바뀔 때 상태 업데이트
   const handleChange = (e) => {
-    setForm({
-      ...form,
+    setForm(prev => ({
+      ...prev,
       [e.target.name]: e.target.value
-    });
+    }));
   };
 
-  //수정하기 메뉴 클릭시 실행되는 함수
+  // 6. “수정하기” 버튼 클릭 시 PUT 요청 (PUT /fruits/update/:num)
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .put(`http://localhost:9070/fruits/update/${num}`, form)
+      .put(`${BACKEND_URL}/fruits/update/${num}`, form)
       .then(() => {
         alert('과일 정보가 수정 완료되었습니다.');
         navigate('/fruits');
       })
-      .catch(err => console.log('수정 오류(fruit) :', err));
+      .catch(err => console.log('수정 오류(fruit):', err));
   };
 
+  // 7. “다시 쓰기 또는 취소” 버튼 클릭 시 폼 초기화
   const handleReset = () => {
-    setForm({ num: form.num, name: '', price: '', color: '', country: '' });
+    setForm({
+      num: form.num,
+      name: '',
+      price: '',
+      color: '',
+      country: ''
+    });
   };
 
   return (
