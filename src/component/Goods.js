6249802a9bf35a5b31d0d6516f8dc0727df2198a
LOO-kees <tlswjs16@gmail.com>
2025-06-05 13:08:13 +0900

    Update Goods.js

--- src/component/Goods.js
+++ src/component/Goods.js
@@ -12,13 +12,15 @@ function Goods(props) {
   const [currentPage, setCurrentPage] = useState(1); //초기값 1
   const itemsPerPage = 7; //한 페이지에 보여지는 게시물 수
 
+   // “절대 URL”을 쓰세요. → 브라우저가 접근 가능한 Public URL
+  const BACKEND_URL = 'https://port-0-backend-mbioc25168a38ca1.sel4.cloudtype.app';
 
   //1. 상품 리스트 조회(출력)
   const loadData = useCallback(() => {
     //React비동기 통신
     axios
       //DB에서 json데이터를 불러온다.
-      .get(`backend/goods`)
+      .get(`${BACKEND_URL}/goods`)
       //성공시 데이터를 변수에 저장
       .then(res => {
         setData(res.data);
@@ -36,7 +38,7 @@ function Goods(props) {
   const deleteData = (g_code) => { //전달받은 g_code값을 가지고  backend로 넘김
     if (window.confirm('정말 삭제하시겠습니까?')) {
       axios //서버에 DELETE 요청 전송
-        .delete(`http://localhost:9070/goods/${g_code}`)
+        .delete(`${BACKEND_URL}/goods/${g_code}`)
         //성공일때 아래 내용 실행
         .then(() => {
           alert('삭제가 완료 되었습니다.');
